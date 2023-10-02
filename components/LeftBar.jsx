d24c0b34266aa523720d4423068beed6bbf302ce
Hitarth <patelhitarth08@gmail.com>
2023-10-03 01:03:51 +0530

    bug fixed

--- components/LeftBar.jsx
+++ components/LeftBar.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import "../styles/leftbar.css";
+import "../styles/leftBar.css";
 
 const LeftBar = () => {
   const [isOpen, setIsOpen] = useState(false);
